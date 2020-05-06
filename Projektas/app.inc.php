c946982c4da3b8ac73007d6025ea42086ccd0fd4
EdvinP <kaidio12@gmail.com>
2020-05-06 21:53:42 +0300

    nd

---
+++ Projektas/app.inc.php
@@ -0,0 +1,19 @@
+<?php
+    if(isset($_POST['submit'])) {
+        include_once('db.inc.php');
+        $vardas = mysqli_real_escape_string($mysqli, $_POST['vardas']);
+        $email = mysqli_real_escape_string ($mysqli, $_POST['email']);
+        $message = mysqli_real_escape_string ($mysqli, $_POST['message']);
+
+        if(empty($vardas) && empty($email) && empty($message)) {
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $from = "$email";
+                $to = "kaidio12@gmail.com";
+                $subject = "Nauja zinute";
+                $autorius = 'Nuo :' .$vardas . ', ' . $email;
+                $zinute = htmlspecialchars($message);
+                mail($to, $subject, $autorius, $zinute, $from);
+                header("Location: ../public/kontaktai.php?mailsend");
+            }
+        }
+    }
