c0bdae673435555faae5bb8abc883928531b0420
EdvinP <kaidio12@gmail.com>
2020-05-06 22:56:52 +0300

    nd

--- Projektas/app.inc.php
+++ Projektas/app.inc.php
@@ -5,15 +5,28 @@
         $email = mysqli_real_escape_string ($mysqli, $_POST['email']);
         $message = mysqli_real_escape_string ($mysqli, $_POST['message']);
 
-        if(!empty($vardas) && !empty($email) && !empty($message)) {
-            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $from = "$email";
-                $to = "kaidio12@gmail.com";
-                $subject = "Nauja zinute";
-                $autorius = 'Nuo :' .$vardas . ', ' . $email;
-                $zinute = htmlspecialchars($message);
-                mail($to, $subject, $autorius, $zinute, $from);
-                header("Location: ../public/kontaktai.php?mailsend");
+        if(empty($vardas) && empty($email) && empty($message)) {
+            header("Locaton: ../public/kontaktai.php?empyfields");
+            exit();
+            }else{
+                if (!preg_match("/^[a-zA-Z]*$/", $vardas)) {
+                    header("Location: ../public/kontaktai.php?wrongname");
+                    exit();
+                }else {
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                 header("Location ../public/kontaktai.php?wrongemail");
+                    exit();
+                }else {
+                    $from = "$email";
+                    $to = "kaidio12@gmail.com";
+                    $subject = "Nauja zinute";
+                    $autorius = 'Nuo :' .$vardas . ', ' . $email;
+                    $zinute = htmlspecialchars($message);
+                    mail($to, $subject, $autorius, $zinute, $from);
+                    header("Location: ../public/kontaktai.php?mailsend");
+                    exit();
+                }
+
             }
         }
     }
